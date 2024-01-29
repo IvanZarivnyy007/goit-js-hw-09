fdfde9b23bda485b3da382c6eefa7221748f0fb0
IvanZarivnyy007 <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-01-29 23:06:28 +0100

    4
    5

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,14 +1,8 @@
 // const STORAGE_KEY = 'feedback-form-state';
-
-let form = document.querySelector('.feedback-form');
-console.log(form);
-
 const localStorageKey = 'feedback-form-state';
 
-let emailOne = document.getElementsByName('email');
-let textarea = document.getElementsByName('message');
-console.log(emailOne);
-console.log(textarea);
+let form = document.querySelector('.feedback-form');
+let textarea = document.querySelector('message');
 
 form.addEventListener('input', onFormInput);
 
@@ -16,24 +10,28 @@ function onFormInput() {
   const email = form.elements.email.value;
   const message = form.elements.message.value;
 
-  let data = {
-    email,
-    message,
-  };
+  //   let data = {
+  //     email,
+  //     message,
+  //   };
 
-  saveInfo('userData', data);
+  //   saveInfo('userData', data);
 }
+// console.log(onFormInput);
 
-function saveInfo(key, value) {
-  const infoJson = JSON.stringify(value);
-  localStorage.setItem('key', 'infoJson');
-}
-function loadInfo(key) {
-  const infoJson = localStorage.setItem('key');
+// function saveInfo(key, value) {
+//   const infoJson = JSON.stringify(value);
+//   localStorage.setItem('key', 'infoJson');
+// }
+// console.log(saveInfo);
 
-  try {
-    return JSON.parse(infoJson);
-  } catch {
-    return infoJson;
-  }
-}
+// function loadInfo(key) {
+//   const infoJson = localStorage.setItem('key');
+
+//   try {
+//     return JSON.parse(infoJson);
+//   } catch {
+//     return infoJson;
+//   }
+// }
+// console.log(loadInfo);
