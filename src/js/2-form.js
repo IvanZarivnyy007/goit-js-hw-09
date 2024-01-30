5aeb685786b62503980458e6191f6a1349bd3647
IvanZarivnyy007 <144140704+IvanZarivnyy007@users.noreply.github.com>
2024-01-30 20:36:01 +0100

    'update'

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,59 +1,44 @@
 const localStorageKey = 'feedback-form-state';
 
 let form = document.querySelector('.feedback-form');
-let textarea = document.querySelector('textarea');
-
-// console.log(form);
-// console.log(textarea);
 
 form.addEventListener('input', onFormInput);
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
-
-  // console.log(email);
-  // console.log(message);
-
-  let data = {
-    email,
-    message,
-  };
-
-  console.log(data);
-  form.reset();
-  localStorage.removeItem(localStorageKey);
+  const email = e.target.elements.email.value;
+  const message = e.target.elements.message.value;
+  if (message && email) {
+    let data = {
+      email,
+      message,
+    };
+    console.log(data);
+    e.target.reset();
+    localStorage.removeItem(localStorageKey);
+  } else {
+    alert('Усі поля форми повинні бути заповненні');
+  }
 }
 
-function onFormInput() {
-  const email = form.elements.email.value;
-  const message = form.elements.message.value;
-
-  console.log(email);
-  console.log(message);
-
+function onFormInput(e) {
+  const email = e.target.form.elements.email.value;
+  const message = e.target.form.elements.message.value;
   let data = {
     email,
     message,
   };
-
-  // console.log(data);
-
   saveInfo(localStorageKey, data);
 }
-// console.log(onFormInput);
 
 function saveInfo(key, value) {
   const infoJson = JSON.stringify(value);
   localStorage.setItem(key, infoJson);
 }
-// console.log(saveInfo);
 
 function loadInfo(key) {
   const infoJson = localStorage.getItem(key);
-
   try {
     return JSON.parse(infoJson);
   } catch (error) {
@@ -61,7 +46,6 @@ function loadInfo(key) {
     return null;
   }
 }
-// console.log(loadInfo);
 
 function finish() {
   const data = loadInfo(localStorageKey) || {};
@@ -70,5 +54,3 @@ function finish() {
 }
 
 finish();
-
-// console.log(form.elements);
